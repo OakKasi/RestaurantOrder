4d34f14dcaf5d8bfe8684023ba8f365827596340
SarawutNn <Sarawut.thong@ku.th>
2026-09-13 17:15:13 +0700

    database

--- src/db/db.js
+++ src/db/db.js
@@ -1,5 +1,54 @@
-import * as SQLite from 'expo-sqlite';
+export const DATABASE_NAME = 'restaurant_db.db'
 
-const db = SQLite.openDatabase('restaurant.db');
+export async function initDb() {
+    await db.execAsync(`
+        PRAGMA foreign_keys = ON;
+        PRAGMA journal_mode = WAL;
 
-export default db;
+        CREATE TABLE IF NOT EXISTS categories (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL
+        );
+        CREATE TABLE IF NOT EXISTS menu_items (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            price REAL NOT NULL,
+            category_id INTEGER NOT NULL,
+
+            FOREIGN KEY (category_id) REFERENCES categories(id)
+
+        );
+        CREATE TABLE IF NOT EXISTS tables (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            table_number TEXT NOT NULL UNIQUE,
+        );
+        CREATE TABLE IF NOT EXISTS bills (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            time_opened TEXT NOT NULL,
+            time_closed TEXT,
+            status TEXT NOT NULL,
+            table_id INTEGER NOT NULL DEFAULT 'OPEN',
+            
+            FOREIGN KEY (table_id) REFERENCES tables(id)
+        );
+        CREATE TABLE IF NOT EXISTS order_rounds (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            round_number INTEGER NOT NULL,
+            ordered_at TEXT NOT NULL,
+            bill_id INTEGER NOT NULL,
+
+            FOREIGN KEY (bill_id) REFERENCES bills(id)
+        );
+        CREATE TABLE IF NOT EXISTS order_items (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            quantity INTEGER NOT NULL,
+            note TEXT,
+            status TEXT NOT NULL,
+            rounds_id INTEGER NOT NULL,
+            menu_item_id INTEGER NOT NULL,
+            
+            FOREIGN KEY (rounds_id) REFERENCES order_rounds(id)
+            FOREIGN KEY (menu_item_id) REFERENCES menu_items(id),
+        )
+    `)
+}
